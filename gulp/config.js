aa82e7ece9b465bb4d96ddfb44221b7216f7735b
echo-cui <33016778+echo-cui@users.noreply.github.com>
2018-05-08 14:05:30 +0800

    汇卡宝商城基础文件

---
+++ gulp/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    //服务器配置
+    serverDev: {
+
+        name: 'jiafu-ticket',//服务名称
+
+        root: 'build',//目录
+
+        host: "192.168.1.186",//ip
+
+        port: 555,//端口
+
+        livereload: true//是否支持实时刷新
+
+    }
+
+};
